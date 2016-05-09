941826d5c6e071c175ceafd29b08236609f187cf
Jeferson de Oliveira Batista <jeferson.de.oliveira.batista@gmail.com>
2016-05-08 23:27:49 -0300

    Atualizando com arquivos que funcionam.

--- Makefile
+++ Makefile
@@ -2,8 +2,7 @@ all: main
 
 main: main.c
 	gcc -Wall -o nsh main.c
-
+	
 clean:
 	rm nsh
-	
-	
+
--- main.c
+++ main.c
@@ -1,18 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 int main() {
-	char programa[51];
-	char argv[2][51];
+	char *argv[2];
+	char program[51], path[51];
 	
-	printf("nsh> ");
-	
-	scanf("%s", programa);
-	argv[0] = "./";
-	argv[0][2] = programa;
-	execvp(programa, NULL);
-	
-	return 0;
+	int pid;
+
+	while(1) {
+		printf("nsh> ");
+		scanf("%s", program);
+		
+		if(strcmp(program, "exit") == 0)
+			return 0;
+			
+		pid = fork();
+		if(pid == 0) {
+			argv[0] = (char*) calloc(strlen(program)+1, sizeof(char));
+			strcpy(argv[0], program);
+			argv[1] = NULL;
+			
+			path[0] = '.';
+			path[1] = '/';
+			path[2] = '\0';
+			strcat(path, program);
+			execvp(path, argv);
+		}
+		
+		if(pid != 0)
+			wait(NULL);
+	}
 }
 
 
